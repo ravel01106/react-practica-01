389b368c871fe6cbfb7cd15d87829ebb23c7fadd
ravel01106 <ravello2002@gmail.com>
2023-05-06 22:41:30 +0100

    feat(): The Footer component is created correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 
 function App () {
   return (
     <div className="App">
       <Header title='Tema 05' subtitle='Práctica 01' />
+      <main>
+        
+      </main>
+      <Footer year={2023}/>
     </div>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import styles from './Footer.module.css';
+import {AiOutlineCopyright} from 'react-icons/ai';
+import propTypes from 'prop-types';
+
+const Footer = ({year}) => {
+  return (
+    <Fragment>
+        <div className={styles.Footer}>
+            <p> <AiOutlineCopyright /> {year} All right reserved </p>
+        </div>
+    </Fragment>
+  )
+}
+Footer.propTypes = {
+    year: propTypes.number.isRequired
+}
+
+export default Footer
