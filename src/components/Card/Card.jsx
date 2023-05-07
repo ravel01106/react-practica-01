eeeb9061ffc8aa444635ea96c4ec067ec7202635
ravel01106 <ravello2002@gmail.com>
2023-05-07 02:35:18 +0100

    feat(Card): The date is formatted correctly

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,24 @@ import styles from './Card.module.css';
 import propTypes from 'prop-types';
 
 const Card = ({character}) => {
-  return (
+    const months = ["January", "February", "March", 
+        "April", "May", "June", "July", "August", "September", 
+        "October", "November", "December"];
+    const days = ["Sunday", "Monday", "Tuesday",
+        "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const formatCreated = () => {
+
+        const dateCharacter = new Date(character.created);
+        const dateFormat = days[dateCharacter.getDay()] + ", " +
+            dateCharacter.getDate() + " de " + months[dateCharacter.getMonth()]
+            + " de " + dateCharacter.getFullYear();
+        return dateFormat;
+
+    }
+
+    return (
+
     <Fragment>
         <div className={styles.Card}>
             <div>
@@ -14,11 +31,12 @@ const Card = ({character}) => {
                 <p>{character.species}</p>
                 <p>{character.gender}</p>
                 <p>{character.location}</p>
-                <p>{character.created}</p>
+                <p>{formatCreated()}</p>
             </div>
         </div>
     </Fragment>
-  )
+    
+    )
 }
 Card.propTypes = {
     character: propTypes.object.isRequired
